54f4be0691568e25b383a7bce09335a982ee075b
Muhammad Akbar <Muhammadakbartr11@gmail.com>
2026-03-29 22:00:16 +0300

    refactor: restored to normal

--- interviewer_chatbot/backend/models/embedding_model.py
+++ interviewer_chatbot/backend/models/embedding_model.py
@@ -1,4 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from config.settings import settings
 
-embeddings = GoogleGenerativeAIEmbeddings(model=settings.gemini_embedding_model)
+embeddings = None
+
+
+def get_embeddings():
+    global embeddings
+    if embeddings is None:
+        embeddings = GoogleGenerativeAIEmbeddings(model=settings.gemini_embedding_model)
+    return embeddings
