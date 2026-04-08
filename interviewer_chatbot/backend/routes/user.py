46d7d25e93fca98bbff1a0e525a638dec16855f6
Muhammad Akbar <Muhammadakbartr11@gmail.com>
2026-04-08 20:12:48 +0300

    refactor: corrected leak

--- interviewer_chatbot/backend/routes/user.py
+++ interviewer_chatbot/backend/routes/user.py
@@ -290,9 +290,9 @@ class EmailSchema(BaseModel):
 
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="Muhammadakbartr11@gmail.com",
-    MAIL_PASSWORD="ktup owro yhpc olqt",
-    MAIL_FROM="Muhammadakbartr11@gmail.com",
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_USERNAME"),
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
